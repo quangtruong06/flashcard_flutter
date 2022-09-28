448e408adae70f66e643fb5465b9b6afa83f1dca
quangtruong06 <106483026+quangtruong06@users.noreply.github.com>
2022-09-28 21:56:52 +0900

    new

--- lib/pages/cardgame_page/component/cardgame_main.dart
+++ lib/pages/cardgame_page/component/cardgame_main.dart
@@ -1,3 +1,4 @@
+import 'package:flashcard_flutter/contain/Globals.dart';
 import 'package:flashcard_flutter/contain/Utils.dart';
 import 'package:flashcard_flutter/contain/response_api.dart';
 import 'package:flashcard_flutter/pages/cardgame_page/model/cardgame_model.dart';
@@ -6,6 +7,7 @@ import 'package:flashcard_flutter/pages/puzzle_game/puzzle_game.dart';
 import 'package:flashcard_flutter/pages/taxonomy_page/model/taxonomy_model.dart';
 import 'package:flashcard_flutter/pages/word_game/word_game.dart';
 import 'package:flashcard_flutter/route_transition/route_transition.dart';
+import 'package:flashcard_flutter/widget/lastpage.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
@@ -53,7 +55,7 @@ class _CardGameBodyState extends State<CardGameBody> {
             ];
             return Center(
               child: CarouselSlider.builder(
-                  itemCount: data.length,
+                  itemCount: data.length+1,
                   options: CarouselOptions(
                     aspectRatio: 16 / 17,
                     autoPlay: false,
@@ -67,50 +69,55 @@ class _CardGameBodyState extends State<CardGameBody> {
                       (BuildContext context, int itemIndex, int pageViewIndex) {
                     return Builder(
                       builder: (BuildContext context) {
-                        return GestureDetector(
-                          onTap: (){
-                            Navigator.push(context, SlideRightRoute(page: data[itemIndex]["nextpage"]));
-                          },
-                          child: Container(
-                              width: MediaQuery.of(context).size.width,
-                              margin:
-                                  const EdgeInsets.symmetric(horizontal: 5.0),
-                              decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(8),
-                                  border: Border.all(color: Colors.black12),
-                                  boxShadow: [
-                                    BoxShadow(
-                                        color: Colors.grey.withOpacity(0.3),
-                                        blurRadius: 10,
-                                        offset: const Offset(2, 10))
-                                  ]),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  itemIndex == 0
-                                      ? Image.network(
-                                          data[0]["img"],
-                                          width: 200,
-                                          height: 200,
-                                          fit: BoxFit.fill,
-                                        )
-                                      : Image.asset(
-                                          data[itemIndex]["img"],
-                                          width: 200,
-                                          height: 200,
-                                          fit: BoxFit.fill,
-                                        ),
-                                  const SizedBox(
-                                    height: 30.0,
-                                  ),
-                                  Utils.customText(
-                                      text: data[itemIndex]["name"],
-                                      size: 30.0,
-                                      fontWeight: FontWeight.bold)
-                                ],
-                              )),
-                        );
+                        if(itemIndex==data.length){
+                          return LastPage(title: CardGameLastPageData().tittle, imageSrc: CardGameLastPageData().imageSrc, description: CardGameLastPageData().description);
+                        }
+                        else{
+                          return GestureDetector(
+                            onTap: (){
+                              Navigator.push(context, SlideRightRoute(page: data[itemIndex]["nextpage"]));
+                            },
+                            child: Container(
+                                width: MediaQuery.of(context).size.width,
+                                margin:
+                                const EdgeInsets.symmetric(horizontal: 5.0),
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(8),
+                                    border: Border.all(color: Colors.black12),
+                                    boxShadow: [
+                                      BoxShadow(
+                                          color: Colors.grey.withOpacity(0.3),
+                                          blurRadius: 10,
+                                          offset: const Offset(2, 10))
+                                    ]),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    itemIndex == 0
+                                        ? Image.network(
+                                      data[0]["img"],
+                                      width: 200,
+                                      height: 200,
+                                      fit: BoxFit.fill,
+                                    )
+                                        : Image.asset(
+                                      data[itemIndex]["img"],
+                                      width: 200,
+                                      height: 200,
+                                      fit: BoxFit.fill,
+                                    ),
+                                    const SizedBox(
+                                      height: 30.0,
+                                    ),
+                                    Utils.customText(
+                                        text: data[itemIndex]["name"],
+                                        size: 30.0,
+                                        fontWeight: FontWeight.bold)
+                                  ],
+                                )),
+                          );
+                        }
                       },
                     );
                   }),
--- lib/widget/lastpage.dart
+++ lib/widget/lastpage.dart
@@ -1,13 +1,57 @@
+import 'package:flashcard_flutter/contain/Utils.dart';
 import 'package:flutter/material.dart';
 
 class LastPage extends StatelessWidget {
-  const LastPage({Key? key}) : super(key: key);
-
+  String? title;
+  String? imageSrc;
+  String? description;
+  String restart = "xem từ đầu";
+  String playGame = "chơi game";
+  LastPage({super.key, required this.title,required this.imageSrc,required this.description});
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-          image: DecorationImage(image: AssetImage("assets/images/win.png"))),
+    return Scaffold(
+      body: Container(
+        height: double.maxFinite,
+        width: double.maxFinite,
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/win.png"), fit: BoxFit.fill)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Utils.customText(text: title!,fontWeight: FontWeight.bold,color: const Color(0xFF3254AC),size: 20.0),
+            const SizedBox(height: 20,),
+            Image.asset(imageSrc!,width: 30,),
+            Utils.customText(text: description!),
+            const SizedBox(height: 20,),
+            Container(
+              height: 40,
+              width: 200,
+              decoration: BoxDecoration(
+                color: Colors.orangeAccent,
+                borderRadius: BorderRadius.circular(12)
+              ),
+              child: Center(
+                child: Utils.customText(text: restart.toUpperCase(),fontWeight: FontWeight.bold,color: Colors.white),
+              ),
+            ),
+            const SizedBox(height: 20,),
+            Container(
+              height: 40,
+              width: 200,
+              decoration: BoxDecoration(
+                  color: Colors.orangeAccent,
+                  borderRadius: BorderRadius.circular(12)
+              ),
+              child: Center(
+                child: Utils.customText(text: playGame.toUpperCase(),fontWeight: FontWeight.bold,color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
